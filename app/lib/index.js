0ea4db8da901f893d398c466b82376d190b92242
jdvega <jdvega@idealista.com>
2017-12-12 10:13:56 +0100

    change chrome version

--- app/lib/index.js
+++ app/lib/index.js
@@ -47,7 +47,7 @@ app.on('ready', () => {
     });
   });
 
-  window.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
+  window.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36');
   window.loadURL('https://teams.microsoft.com/');
 
   if (process.env.WEB_DEBUG) {
